6b8f442c01a2a6a10e7e7b134cdd598dc86861b7
Zintarh <kateberryd@gmail.com>
2025-04-17 16:56:27 +0100

    fix: fix UI

--- src/app/hooks/argentSDKWallet/useArgentWebWallet.ts
+++ src/app/hooks/argentSDKWallet/useArgentWebWallet.ts
@@ -5,7 +5,7 @@ export default function useArgentWebWallet() {
   const argentWebWallet = useMemo(
     () =>
       ArgentWebWallet.init({
-        appName: 'coloniz.xyz',
+        appName: 'scanguard.xyz',
         environment: 'sepolia',
         paymasterParams: {
           apiKey: '874b0d50-e322-4cd4-ad3c-e8810b3d37f0',
